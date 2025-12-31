0ea2cbbb936b510166475e843e9ea1407907add7
rachit-developer24 <srachit841@gmail.com>
2025-12-31 22:55:48 +0000

    Initial Commit

---
+++ AirBnbProject/AirBnbProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbProjectApp.swift
+//  AirBnbProject
+//
+//  Created by Rachit Sharma on 31/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirBnbProjectUITests/AirBnbProjectUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AirBnbProjectUITestsLaunchTests.swift
+//  AirBnbProjectUITests
+//
+//  Created by Rachit Sharma on 31/12/2025.
+//
+
+import XCTest
+
+final class AirBnbProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
